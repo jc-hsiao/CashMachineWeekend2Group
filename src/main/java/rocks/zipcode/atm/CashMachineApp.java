d1ee20eb46ca4cdce2d483787fa7800c498df3fc
Leila Hsiao <lsiao@zipcodes-MacBook-Pro-6.local>
2020-02-22 11:52:28 -0500

    orgnize the structure of panels being generated

--- src/main/java/rocks/zipcode/atm/CashMachineApp.java
+++ src/main/java/rocks/zipcode/atm/CashMachineApp.java
@@ -67,60 +67,115 @@ public class CashMachineApp extends Application {
 //    }
 
 
+    VBox vbox = new VBox(10);
+    GridPane loginGrid = new GridPane();
+    GridPane mainGrid = new GridPane();
 
-    private Parent createPage() {
-        VBox vbox = new VBox(10);
+    private void setUpUI(){
         vbox.setPrefSize(500, 500);
-        vbox.setPadding(new Insets(110, 70,70,70));
-        //vbox.setBackground(new Background(new BackgroundImage()));
-        //vbox.setSpacing(10);
+        vbox.setPadding(new Insets(70, 50,70,50));
 
-        GridPane grid = new GridPane();
-        grid.setId("grid");
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(20);
-        grid.setPadding(new Insets(40,20,40,20));
+        setUpLoginGrid();
+        setUpMainGrid();
+
+        vbox.getChildren().add(loginGrid);
+    }
+
+    private void setUpLoginGrid(){
+        loginGrid.setId("grid");
+        loginGrid.setAlignment(Pos.CENTER);
+        loginGrid.setHgap(10);
+        loginGrid.setVgap(20);
+        loginGrid.setPadding(new Insets(40,20,40,20));
 
         Text header = new Text("ZipCloudBank");
-        header.setId("header");
         Label logInTxt1 = new Label("Account ID: ");
         Label logInTxt2 = new Label("PIN number: ");
         TextField field = new TextField();
         PasswordField field2 = new PasswordField();
+        Button loginBT= new Button("Log in");
+        HBox BTBox = new HBox(10);
+        Text oops = new Text();
+
+        header.setId("header");
         field.setPrefColumnCount(15);
         field2.setPrefColumnCount(15);
 
-        Button loginBT= new Button("Log in");
-        HBox BTBox = new HBox(10);
+        loginBT.setId("loginBT");
         BTBox.setAlignment(Pos.BOTTOM_RIGHT);
-        Text oops = new Text();
         oops.setId("warning");
         BTBox.getChildren().add(oops);
         BTBox.getChildren().add(loginBT);
-
         loginBT.setOnAction(e -> {
-           //vbox.getChildren().clear();
-           oops.setText("Oops message!");
+            vbox.getChildren().clear();
+            vbox.getChildren().add(mainGrid);
+            //oops.setText("Oops message!");
         });
 
+        loginGrid.add(header, 0,0,2,1);
+        loginGrid.add(logInTxt1,0,1);
+        loginGrid.add(field,1,1);
+        loginGrid.add(logInTxt2,0,2);
+        loginGrid.add(field2,1,2);
+        loginGrid.add(BTBox, 1,3);
 
-        grid.add(header, 0,0,2,1);
-        grid.add(logInTxt1,0,1);
-        grid.add(field,1,1);
-        grid.add(logInTxt2,0,2);
-        grid.add(field2,1,2);
-        grid.add(BTBox, 1,3);
-        vbox.getChildren().addAll(grid);
-
-        return vbox;
     }
 
+
+    private void setUpMainGrid(){
+        mainGrid.setId("grid");
+        mainGrid.setAlignment(Pos.CENTER);
+        mainGrid.setHgap(10);
+        mainGrid.setVgap(10);
+        mainGrid.setPadding(new Insets(40,20,40,20));
+
+        Text greetTxt = new Text("Hi, regular user");
+        Text balanceTxt = new Text("Your balance is 1000");
+        TextField numField = new TextField();
+        Button withdrawBT= new Button("Withdraw");
+        Button depositBT= new Button("Deposit");
+        Button logOutBT= new Button("Log Out");
+        HBox BTBox2 = new HBox(10);
+        Text oops2 = new Text();
+
+        numField.setPrefSize(300,50);
+        numField.setAlignment(Pos.CENTER_RIGHT);
+        numField.setId("numField");
+
+        withdrawBT.setPrefSize(90,50);
+        depositBT.setPrefSize(90,50);
+        withdrawBT.setId("wBT");
+        depositBT.setId("dBT");
+        logOutBT.setId("outBT");
+        logOutBT.setOnAction(e -> {
+            vbox.getChildren().clear();
+            vbox.getChildren().add(loginGrid);
+        });
+
+        BTBox2.getChildren().add(logOutBT);
+        BTBox2.setAlignment(Pos.CENTER_RIGHT);
+        oops2.setId("warning");
+        withdrawBT.setOnAction(e -> {
+            //
+        });
+        depositBT.setOnAction(e -> {
+            oops2.setText("Oops message!\n asj lasjdl aksjd a");
+        });
+
+        mainGrid.add(greetTxt,0,0);
+        mainGrid.add(balanceTxt,0,1);
+        mainGrid.add(numField,0,3,3,1);
+        mainGrid.add(oops2,0,4);
+        mainGrid.add(withdrawBT,1,4);
+        mainGrid.add(depositBT,2,4);
+        mainGrid.add(BTBox2,2,6);
+    }
 
 
     @Override
     public void start(Stage stage) throws Exception {
-        Scene scene = new Scene(createPage());
+        setUpUI();
+        Scene scene = new Scene(vbox);
         stage.setScene(scene);
         scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
         stage.show();
