5179cd8d77813b4647fe39a2f4bed4dcfb66c0b0
Leila Hsiao <lsiao@zipcodes-MacBook-Pro-6.local>
2020-02-22 18:09:58 -0500

    start making a mock premium page UI

--- src/main/java/rocks/zipcode/atm/CashMachineApp.java
+++ src/main/java/rocks/zipcode/atm/CashMachineApp.java
@@ -25,6 +25,7 @@ public class CashMachineApp extends Application {
     VBox vbox = new VBox(10);
     GridPane loginGrid = new GridPane();
     GridPane mainGrid = new GridPane();
+    GridPane premGrid = new GridPane();
 
     private void setUpUI(){
         vbox.setPrefSize(500, 500);
@@ -32,8 +33,9 @@ public class CashMachineApp extends Application {
 
         setUpLoginGrid();
         setUpMainGrid();
+        setUpMainGrid2();
 
-        vbox.getChildren().add(loginGrid);
+        vbox.getChildren().add(premGrid);
 
     }
 
@@ -204,6 +206,129 @@ public class CashMachineApp extends Application {
 
         //mainGrid.setGridLinesVisible(true);
     }
+
+
+
+    private void setUpMainGrid2(){
+        premGrid.setId("premGrid");
+        premGrid.setAlignment(Pos.CENTER);
+        premGrid.setHgap(10);
+        premGrid.setVgap(5);
+        premGrid.setPadding(new Insets(40,20,40,20));
+
+        Text greetTxt = new Text("Hi, regular user");
+        Text balanceTxt = new Text("Your current balance is: ");
+        Label balanceNum = new Label("$1000.00");
+        Button withdrawBT= new Button("Withdraw");
+        Button depositBT= new Button("Deposit");
+        Button logOutBT= new Button("Log Out");
+        HBox buttonHBox = new HBox(10);
+        HBox logOutHBox = new HBox(10);
+        HBox warningHBox = new HBox(10);
+        Text oops2 = new Text();
+        Stage moneyStage = new Stage();
+        VBox moneyVBox = new VBox();
+        TextField moneyField = new TextField();
+
+        Image insertImg = new Image("insert.gif");
+        Image withdrawImg = new Image("withdraw.gif");
+        Image badImg = new Image("nope.jpg");
+
+        Button doneInsert = new Button("Deposit");
+        Button doneWithdraw = new Button("Withdraw");
+
+
+        greetTxt.setId("greetTxt");
+        balanceTxt.setId("balanceTxt");
+
+        balanceNum.setPrefSize(300,50);
+        balanceNum.setAlignment(Pos.CENTER_RIGHT);
+        balanceNum.setId("balanceNum");
+
+        withdrawBT.setPrefSize(90,50);
+        depositBT.setPrefSize(90,50);
+
+        withdrawBT.setId("wBT");
+        depositBT.setId("dBT");
+        logOutBT.setId("outBT");
+        logOutBT.setOnAction(e -> {
+            oops2.setText("");
+            vbox.getChildren().clear();
+            vbox.getChildren().add(loginGrid);
+        });
+
+        buttonHBox.getChildren().add(withdrawBT);
+        buttonHBox.getChildren().add(depositBT);
+        buttonHBox.setAlignment(Pos.CENTER_RIGHT);
+        buttonHBox.setPadding(new Insets(10,0, 10,0));
+        oops2.setId("warning");
+
+        //pop up
+        Popup popup = new Popup();
+        moneyVBox.setId("moneyVBox");
+        moneyField.setId("moneyField");
+        moneyField.setPrefWidth(220);
+        doneInsert.setPrefSize(220,30);
+        doneWithdraw.setPrefSize(220,30);
+        Scene moneyScene = new Scene(moneyVBox);
+        moneyScene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+
+        withdrawBT.setOnAction(e -> {
+            oops2.setText("");
+            if (!popup.isShowing()){
+                moneyVBox.getChildren().clear();
+                moneyStage.setTitle("Please Enter Amount");
+                ImageView imageView1 = new ImageView(withdrawImg);
+                moneyVBox.getChildren().addAll(imageView1,moneyField,doneWithdraw);
+                moneyStage.setScene(moneyScene);
+                moneyStage.show();
+                popup.show(moneyStage);
+            } else
+                popup.hide();
+        });
+        doneWithdraw.setOnAction(e -> {
+            oops2.setText("You don't have enough money!");
+            moneyStage.close();
+
+        });
+
+
+        depositBT.setOnAction(e -> {
+            oops2.setText("");
+            if (!popup.isShowing()){
+                moneyVBox.getChildren().clear();
+                moneyStage.setTitle("Please Insert Money");
+                ImageView imageView2 = new ImageView(insertImg);
+                moneyVBox.getChildren().addAll(imageView2,moneyField,doneInsert);
+                moneyStage.setScene(moneyScene);
+                moneyStage.show();
+                popup.show(moneyStage);
+            } else
+                popup.hide();
+        });
+        doneInsert.setOnAction(e -> {
+
+            //this will close the window
+            moneyStage.close();
+        });
+
+        logOutHBox.setAlignment(Pos.CENTER_RIGHT);
+        logOutHBox.getChildren().add(logOutBT);
+        warningHBox.setAlignment(Pos.CENTER_RIGHT);
+        warningHBox.setPadding(new Insets(0,5,0,0));
+        warningHBox.getChildren().add(oops2);
+
+
+        premGrid.add(greetTxt,0,0);
+        premGrid.add(balanceTxt,0,2);
+        premGrid.add(balanceNum,0,4);
+        premGrid.add(buttonHBox,0,5);
+        premGrid.add(warningHBox,0,6);
+        premGrid.add(logOutHBox,0,7);
+
+        //mainGrid.setGridLinesVisible(true);
+    }
+
 
 
     @Override
